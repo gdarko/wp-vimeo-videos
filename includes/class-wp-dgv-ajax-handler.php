79bd2b4d6b1d843f961d6c26bd9809fc44a3b002
Darko Gjorgjijoski <dg@darkog.com>
2021-02-21 18:48:43 +0000

    Fix missing space

--- includes/class-wp-dgv-ajax-handler.php
+++ includes/class-wp-dgv-ajax-handler.php
@@ -4,7 +4,7 @@
  *
  * This file is part of WP Vimeo Videos
  *
- *WP Vimeo Videos is free software: you can redistribute it and/or modify
+ * WP Vimeo Videos is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2 of the License, or
  * (at your option) any later version.
