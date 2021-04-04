2823a31dddac0247a41dbc447dfaf09c9490497a
Darko Gjorgjijoski <dg@darkog.com>
2021-04-04 21:43:49 +0000

    Added size column

--- includes/class-wp-dgv-list-table.php
+++ includes/class-wp-dgv-list-table.php
@@ -33,7 +33,7 @@ if ( ! class_exists( 'WP_List_Table' ) ) {
  */
 class WP_DGV_List_Table extends \WP_List_Table {
 
-    protected $author_uploads_only;
+	protected $author_uploads_only;
 
 	/**
 	 * @var WP_DGV_Db_Helper
@@ -45,14 +45,14 @@ class WP_DGV_List_Table extends \WP_List_Table {
 	 */
 	protected $api_helper;
 
-    /**
-     * @var WP_DGV_Settings_Helper
-     */
+	/**
+	 * @var WP_DGV_Settings_Helper
+	 */
 	protected $settings_helper;
 
-    /**
-     * WP_DGV_List_Table constructor.
-     */
+	/**
+	 * WP_DGV_List_Table constructor.
+	 */
 	public function __construct() {
 		parent::__construct( array(
 			'singular' => 'video',
@@ -60,15 +60,15 @@ class WP_DGV_List_Table extends \WP_List_Table {
 			'ajax'     => false
 		) );
 
-		$this->settings_helper = new WP_DGV_Settings_Helper();
-		$this->author_uploads_only = $this->settings_helper->get('dgv_author_uploads_only');
-		$this->db_helper  = new WP_DGV_Db_Helper();
-		$this->api_helper = new WP_DGV_Api_Helper();
+		$this->settings_helper     = new WP_DGV_Settings_Helper();
+		$this->author_uploads_only = $this->settings_helper->get( 'dgv_author_uploads_only' );
+		$this->db_helper           = new WP_DGV_Db_Helper();
+		$this->api_helper          = new WP_DGV_Api_Helper();
 	}
 
-    /**
-     * @return array|string[]
-     */
+	/**
+	 * @return array|string[]
+	 */
 	public function get_table_classes() {
 		return array( 'widefat', 'fixed', 'striped', $this->_args['plural'] );
 	}
@@ -96,11 +96,21 @@ class WP_DGV_List_Table extends \WP_List_Table {
 		switch ( $column_name ) {
 			case 'embed':
 				$vimeo_id = $this->db_helper->get_vimeo_id( $item->ID );
+
 				return '<code>[dgv_vimeo_video id="' . $vimeo_id . '"]</code>';
 			case 'author':
 				$user      = get_user_by( 'id', $item->post_author );
-				$user_name = ($user instanceof WP_User && $user->exists()) ? $user->display_name : 'Unknown';
+				$user_name = ( $user instanceof WP_User && $user->exists() ) ? $user->display_name : 'Unknown';
+
 				return $user_name;
+			case 'size':
+				$size = get_post_meta( $item->ID, 'dgv_size', true );
+				if(is_numeric($size)) {
+					$size = wvv_format_bytes($size);
+                } else {
+				    $size = __('Not available', 'wp-vimeo-videos');
+                }
+				return $size;
 			case 'uploaded_at':
 				return get_the_date( get_option( 'date_format' ), $item );
 			default:
@@ -119,6 +129,7 @@ class WP_DGV_List_Table extends \WP_List_Table {
 			'title'       => __( 'Title', 'wp-vimeo-videos' ),
 			'embed'       => __( 'Embed', 'wp-vimeo-videos' ),
 			'author'      => __( 'Author', 'wp-vimeo-videos' ),
+			'size'        => __( 'Size', 'wp-vimeo-videos' ),
 			'uploaded_at' => __( 'Uploaded', 'wp-vimeo-videos' ),
 		);
 
@@ -133,13 +144,12 @@ class WP_DGV_List_Table extends \WP_List_Table {
 	 * @return string
 	 */
 	public function column_title( $item ) {
-		$actions           = array();
-		$url               = admin_url( 'upload.php?page=' . WP_DGV_Admin::PAGE_VIMEO . '&action=edit&id=' . $item->ID );
-		$vimeo_uri  = $this->db_helper->get_vimeo_uri($item->ID);
-		$vimeo_id   = $this->db_helper->get_vimeo_id($item->ID);
-		$vimeo_link       = 'https://vimeo.com/'.$vimeo_id;
-		$actions['edit']   = sprintf( '<a href="%s" data-id="%d" title="%s">%s</a>', $url, $item->ID, __( 'Manage this video', 'wp-vimeo-videos' ), __( 'Manage', 'wp-vimeo-videos' ) );
-		$actions['vimeo'] = sprintf('<a href="%s" target="_blank" data-id="%d" title="%s">%s</a>', $vimeo_link, $item->ID, __('Vimeo video link', 'wp-vimeo-videos'), __('Vimeo Link', 'wp-vimeo-videos'));
+		$actions          = array();
+		$url              = admin_url( 'upload.php?page=' . WP_DGV_Admin::PAGE_VIMEO . '&action=edit&id=' . $item->ID );
+		$vimeo_id         = $this->db_helper->get_vimeo_id( $item->ID );
+		$vimeo_link       = 'https://vimeo.com/' . $vimeo_id;
+		$actions['edit']  = sprintf( '<a href="%s" data-id="%d" title="%s">%s</a>', $url, $item->ID, __( 'Manage this video', 'wp-vimeo-videos' ), __( 'Manage', 'wp-vimeo-videos' ) );
+		$actions['vimeo'] = sprintf( '<a href="%s" target="_blank" data-id="%d" title="%s">%s</a>', $vimeo_link, $item->ID, __( 'Vimeo video link', 'wp-vimeo-videos' ), __( 'Vimeo Link', 'wp-vimeo-videos' ) );
 
 		return sprintf( '<a href="%1$s"><strong>%2$s</strong></a> %3$s', $url, $item->post_title, $this->row_actions( $actions ) );
 	}
@@ -215,11 +225,11 @@ class WP_DGV_List_Table extends \WP_List_Table {
 			'offset' => $offset,
 			'number' => $per_page,
 		);
-		if($this->author_uploads_only && !current_user_can('administrator')) {
+		if ( $this->author_uploads_only && ! current_user_can( 'administrator' ) ) {
 			$args['author'] = get_current_user_id();
 		} else {
-			$filter_author_ID = isset($_POST['author']) ? $_POST['author'] : 0;
-			if($filter_author_ID > 0) {
+			$filter_author_ID = isset( $_POST['author'] ) ? $_POST['author'] : 0;
+			if ( $filter_author_ID > 0 ) {
 				$args['author'] = $filter_author_ID;
 			}
 		}
@@ -238,12 +248,13 @@ class WP_DGV_List_Table extends \WP_List_Table {
 	/**
 	 * Generate the table navigation above or below the table
 	 *
-	 * @since 3.1.0
 	 * @param string $which
+	 *
+	 * @since 3.1.0
 	 */
 	protected function display_tablenav( $which ) {
 
-		if($this->author_uploads_only && !current_user_can('administrator')) {
+		if ( $this->author_uploads_only && ! current_user_can( 'administrator' ) ) {
 			return;
 		}
 
@@ -251,47 +262,53 @@ class WP_DGV_List_Table extends \WP_List_Table {
 			wp_nonce_field( 'bulk-' . $this->_args['plural'] );
 		}
 		?>
-		<div class="tablenav <?php echo esc_attr( $which ); ?>">
+        <div class="tablenav <?php echo esc_attr( $which ); ?>">
 
 			<?php
 			$this->extra_tablenav( $which );
 			$this->pagination( $which );
 			?>
-			<br class="clear" />
-		</div>
+            <br class="clear"/>
+        </div>
 		<?php
 	}
 
-    /**
-     * Extra controls to be displayed between bulk actions and pagination
-     *
-     * @since 3.1.0
-     *
-     * @param string $which
-     */
-    protected function extra_tablenav( $which ) {
-        if ( $which == "top" ) {
-            ?>
+	/**
+	 * Extra controls to be displayed between bulk actions and pagination
+	 *
+	 * @param string $which
+	 *
+	 * @since 3.1.0
+	 *
+	 */
+	protected function extra_tablenav( $which ) {
+		if ( $which == "top" ) {
+			?>
             <div class="alignleft actions bulkactions">
-                <?php
-                $filter_author = false;
-                $filter_author_ID = isset($_REQUEST['author']) ? intval($_REQUEST['author']) : 0;
-                if($filter_author_ID) {
-                    $filter_author = get_user_by('id', $filter_author_ID);
-                }
-                ?>
+				<?php
+				$filter_author    = false;
+				$filter_author_ID = isset( $_REQUEST['author'] ) ? intval( $_REQUEST['author'] ) : 0;
+				if ( $filter_author_ID ) {
+					$filter_author = get_user_by( 'id', $filter_author_ID );
+				}
+				?>
                 <div class="alignleft actions">
-                    <label class="screen-reader-text" for="author"><?php __('Filter by author', 'wp-vimeo-videos'); ?></label>
-                    <select name="author" id="author" class="postform dgv-select2" data-placeholder="<?php _e('Filter by author'); ?>">
-                        <?php if(!empty($filter_author)): ?>
-                            <option selected value="<?php echo $filter_author->ID; ?>"><?php echo $filter_author->display_name; ?></option>
-                        <?php endif; ?>
+                    <label class="screen-reader-text"
+                           for="author"><?php __( 'Filter by author', 'wp-vimeo-videos' ); ?></label>
+                    <select name="author" id="author" class="postform dgv-select2"
+                            data-placeholder="<?php _e( 'Filter by author' ); ?>">
+						<?php if ( ! empty( $filter_author ) ): ?>
+                            <option selected
+                                    value="<?php echo $filter_author->ID; ?>"><?php echo $filter_author->display_name; ?></option>
+						<?php endif; ?>
                     </select>
-                    <input type="submit" name="filter_action" id="post-query-submit" class="button-primary" value="Filter">
-                    <a href="" class="dgv-clear-selection" data-target=".dgv-select2" style="<?php echo $filter_author ? '' : 'display:none;'; ?>"><?php _e('Clear'); ?></a>
+                    <input type="submit" name="filter_action" id="post-query-submit" class="button-primary"
+                           value="Filter">
+                    <a href="" class="dgv-clear-selection" data-target=".dgv-select2"
+                       style="<?php echo $filter_author ? '' : 'display:none;'; ?>"><?php _e( 'Clear' ); ?></a>
                 </div>
             </div>
-            <?php
-        }
-    }
+			<?php
+		}
+	}
 }
