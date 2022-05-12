5968c504a8a283be861c1a5404ee3ff497a5d898
Darko Gjorgjijoski <dg@darkog.com>
2022-05-12 09:06:53 +0000

    Add php-curl dependency check

--- wp-vimeo-videos.php
+++ wp-vimeo-videos.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Video Uploads for Vimeo
  * Plugin URI:        https://codeverve.com
  * Description:       Embed and upload videos to Vimeo directly from WordPress
- * Version:           1.8.0
+ * Version:           1.8.2
  * Author:            Darko Gjorgjijoski
  * Author URI:        https://darkog.com
  * License:           GPL-2.0+
@@ -31,11 +31,33 @@ if ( ! is_plugin_active( 'wp-vimeo-videos-pro/wp-vimeo-videos-pro.php' ) ) {
     define('WP_VIMEO_VIDEOS_BASENAME', plugin_basename(__FILE__));
     define('WP_VIMEO_VIDEOS_MIN_PHP_VERSION', '5.5.0');
 
+	/**
+	 * Dependency check.
+	 */
+	if ( ! function_exists( 'curl_version' ) ) {
+		add_action( 'admin_notices', function () {
+			$class   = 'notice notice-error is-dismissible';
+			$plugin = 'Video Uploads for Vimeo';
+			$message = __( 'The cURL PHP extension is required in order to run this plugin. Please contact your webhost to enable the cURL PHP extension.', 'wp-vimeo-videos' );
+			printf( '<div class="%1$s"><p><strong>%2$s</strong>: %3$s</p></div>', esc_attr( $class ), esc_html($plugin), esc_html( $message ) );
+		} );
+
+		return;
+	}
+
+	/**
+	 * Run on plugin acitvation
+	 * @return void
+	 */
 	function activate_wp_vimeo_videos() {
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-dgv-activator.php';
 		WP_DGV_Activator::activate();
 	}
 
+	/**
+	 * Run on plugin deactivation
+	 * @return void
+	 */
 	function deactivate_wp_vimeo_videos() {
 		require_once plugin_dir_path( __FILE__ ) . 'includes/class-wp-dgv-deactivator.php';
 		WP_DGV_Deactivator::deactivate();
@@ -43,13 +65,17 @@ if ( ! is_plugin_active( 'wp-vimeo-videos-pro/wp-vimeo-videos-pro.php' ) ) {
 
 	register_activation_hook( __FILE__, 'activate_wp_vimeo_videos' );
 	register_deactivation_hook( __FILE__, 'deactivate_wp_vimeo_videos' );
+
+
+	/**
+	 * Bootstrap the plugin
+	 */
 	require plugin_dir_path( __FILE__ ) . 'includes/class-wp-dgv.php';
 	function run_wp_vimeo_videos() {
-
 		$plugin = new WP_DGV();
 		$plugin->run();
-
 	}
 
 	run_wp_vimeo_videos();
+
 }
