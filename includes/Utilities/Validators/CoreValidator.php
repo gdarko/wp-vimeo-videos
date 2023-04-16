859ba631446f8291e4bbf9f7470005b88d7cdaea
Darko Gjorgjijoski <dg@darkog.com>
2023-04-16 10:00:00 +0200

    Add is_function_vailable to CoreValidator

--- includes/Utilities/Validators/CoreValidator.php
+++ includes/Utilities/Validators/CoreValidator.php
@@ -13,4 +13,34 @@ class CoreValidator {
 		return version_compare( PHP_VERSION, $version, '>=' );
 	}
 
+	/**
+	 * Check if function is available
+	 *
+	 * @param $name
+	 *
+	 * @return bool
+	 *
+	 * @since 1.6.0
+	 */
+	public function is_function_available( $name ) {
+		static $available;
+		if ( ! isset( $available ) ) {
+			if ( ! function_exists( $name ) ) {
+				$available = false;
+			} else {
+				$available = true;
+				$d         = ini_get( 'disable_functions' );
+				$s         = ini_get( 'suhosin.executor.func.blacklist' );
+				if ( "$d$s" ) {
+					$array = preg_split( '/,\s*/', "$d,$s" );
+					if ( in_array( $name, $array ) ) {
+						$available = false;
+					}
+				}
+			}
+		}
+
+		return $available;
+	}
+
 }
