cbb16405bc98e10613adecc703cfd313884f23f8
Darko Gjorgjijoski <dg@darkog.com>
2023-11-05 15:05:44 +0100

    Add release scripts

---
+++ scripts/release_prepare.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(
+  cd -- "$(dirname "$0")" >/dev/null 2>&1
+  pwd -P
+)"
+PLUGIN_DIR="$(
+  cd -- "$(dirname "$SCRIPT_DIR")" >/dev/null 2>&1
+  pwd -P
+)"
+PLUGINS_ROOT_DIR="$(
+  cd -- "$(dirname "$PLUGIN_DIR")" >/dev/null 2>&1
+  pwd -P
+)"
+PLUGIN_SLUG=$(basename $PLUGIN_DIR)
+
+if [[ -f "$PLUGIN_DIR/composer.json" ]]; then
+  rm -rf "$PLUGIN_DIR/vendor"
+  composer install --no-dev
+fi
+
+if [ -f "$PLUGINS_ROOT_DIR/$PLUGIN_SLUG.zip" ]; then
+  rm "$PLUGINS_ROOT_DIR/$PLUGIN_SLUG.zip"
+fi
+
+cd "$PLUGIN_DIR"
+rm -rf node_modules
+rm -rf package-lock.json
+npm install --legacy-peer-deps
+npm run prod
+if [ ! -d "$PLUGIN_DIR/blocks/dist" ] OR [ ! -d "$PLUGIN_DIR/assets/admin/dist" ]; then
+  echo "Unable to build with NPM"
+  exit
+fi
+
+cd "$PLUGINS_ROOT_DIR"
+
+zip -r "$PLUGIN_SLUG.zip" "$PLUGIN_SLUG" \
+  -x="*scripts*" \
+  -x="*assets/admin/src*" \
+  -x="*assets/shared/src*" \
+  -x="*assets/frontend/src*" \
+  -x="*blocks/src*" \
+  -x="*blocks/node_modules*" \
+  -x="*blocks/package.json*" \
+  -x="*blocks/package-lock.json*" \
+  -x="*.git*" \
+  -x="*package.json*" \
+  -x="*package-lock.json*" \
+  -x="*gulpfile.js*" \
+  -x="*composer.json*" \
+  -x="*composer.lock*" \
+  -x="*README.md*"
+
+echo "New version ready."
