974b87ba9c2d71d55136b20fb1162559c4436866
Darko Gjorgjijoski <dg@darkog.com>
2023-11-05 14:49:22 +0100

    Remove ignored block dist files

--- blocks/dist/video/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element'), 'version' => '4c2f7f61d4e20b423cc6');
--- blocks/dist/videos-table/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b65a3232e81f2f2fe76');
